b487dde078945c594ae791b07326549ade48b3e9
Kristian Mandrup <kmandrup@gmail.com>
2011-09-06 20:58:27 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jegerkreditor::Application.config.secret_token = '643d54f358a13a43bb0723827e89e04d1afdeb19891059ce35b720e5a5e4370d407a6bf570da3ced0c6a3655cf4e5ea713b896dde3b89cf2764b7e2f62ad2faa'
